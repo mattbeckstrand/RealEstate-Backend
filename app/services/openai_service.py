01e2378f52399eb5f97ae5be19093bb3dd42609d
Matt Beckstrand <matt.beckstrand@gmail.com>
2025-02-18 23:01:40 -0700

    commented out open ai stuff for now

--- app/services/openai_service.py
+++ app/services/openai_service.py
@@ -1,26 +1,26 @@
-from openai import OpenAI
-import os
-from dotenv import load_dotenv
+# from openai import OpenAI
+# import os
+# from dotenv import load_dotenv
 
-load_dotenv()
+# load_dotenv()
 
-# Initialize the client with your API key
-api_key = os.getenv('OPENAI_API_KEY')
-if not api_key:
-    raise ValueError("OpenAI API key not found in environment variables")
+# # Initialize the client with your API key
+# api_key = os.getenv('OPENAI_API_KEY')
+# if not api_key:
+#     raise ValueError("OpenAI API key not found in environment variables")
 
-client = OpenAI(
-    api_key=api_key
-)
-completion = client.chat.completions.create(
-    model="gpt-4o-mini",
-    messages=[
-        {"role": "system", "content": "You are a helpful assistant."},
-        {
-            "role": "user",
-            "content": "Write a haiku about recursion in programming."
-        }
-    ]
-)
+# client = OpenAI(
+#     api_key=api_key
+# )
+# completion = client.chat.completions.create(
+#     model="gpt-4o-mini",
+#     messages=[
+#         {"role": "system", "content": "You are a helpful assistant."},
+#         {
+#             "role": "user",
+#             "content": "Write a haiku about recursion in programming."
+#         }
+#     ]
+# )
 
-print(completion.choices[0].message)
+# print(completion.choices[0].message)
